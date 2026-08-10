ba06582eb7723cd1ee84847f4b00af7cf4fd046c
xiejinlong <303182519@qq.com>
2026-08-10 15:38:52 +0800

    feat(pipe): 新增验证管道以支持参数验证

---
+++ src/common/pipes/validation.pipe.ts
@@ -0,0 +1,35 @@
+import {
+  ArgumentMetadata,
+  BadRequestException,
+  Injectable,
+  PipeTransform,
+  Type,
+} from '@nestjs/common';
+import { plainToInstance } from 'class-transformer';
+import { validate } from 'class-validator';
+
+// 提醒一下，你不必自己构建通用验证管道，因为 ValidationPipe 由 Nest 开箱即用。
+// 下面只是个例子： https://xiguadev.com/node-60days/day-18
+@Injectable()
+export class ValidationPipe<T> implements PipeTransform<T> {
+  async transform(value: T, { metatype }: ArgumentMetadata) {
+    if (!metatype || !this.toValidate(metatype)) {
+      return value;
+    }
+
+    const object = plainToInstance(metatype, value) as object;
+    const errors = await validate(object);
+
+    if (errors.length > 0) {
+      throw new BadRequestException('参数验证失败!');
+    }
+
+    return value;
+  }
+
+  private toValidate(metatype: Type<any>): boolean {
+    const types: Type<any>[] = [String, Boolean, Number, Array, Object];
+
+    return !types.includes(metatype);
+  }
+}
