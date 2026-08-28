f9ede56c38c96104a5a02f4644e44e92a5b5a514
xiejinlong <303182519@qq.com>
2026-08-28 10:30:01 +0800

    chore(auth): 清理未使用的认证异常常量和映射
    移除了废弃的UNAUTHORIZED异常码及相关配置，删除jwt策略文件中多余的空行

--- src/common/exceptions/auth.exception.ts
+++ src/common/exceptions/auth.exception.ts
@@ -3,7 +3,6 @@ import { ExceptionInfo } from './base.exception';
 
 export const AuthExceptionCode = {
   INVALID_CREDENTIALS: '10401',
-  UNAUTHORIZED: '10402',
   INVALID_REFRESH_TOKEN: '10403',
   ACCOUNT_LOCKED: '10404',
 } as const;
@@ -16,11 +15,6 @@ export const AuthExceptionMap: Record<AuthExceptionCode, ExceptionInfo> = {
     message: '用户名或密码错误',
     status: HttpStatus.UNAUTHORIZED,
     code: AuthExceptionCode.INVALID_CREDENTIALS,
-  },
-  [AuthExceptionCode.UNAUTHORIZED]: {
-    message: '未授权访问',
-    status: HttpStatus.UNAUTHORIZED,
-    code: AuthExceptionCode.UNAUTHORIZED,
   },
   [AuthExceptionCode.INVALID_REFRESH_TOKEN]: {
     message: '无效的刷新令牌',
